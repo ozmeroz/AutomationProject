4807b24ea729606575558042c48ff73f01678133
Oz Meroz <ozmeroz@gmail.com>
2021-01-13 15:23:59 +0200

    .

--- Pages/mainPage.py
+++ Pages/mainPage.py
@@ -8,4 +8,9 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.action_chains import ActionChains
 class mainPage:
 
-    def
+    def usermenu(self):
+        return self.driver.find_element_by_css_selector("#menuUser")
+    def usernameField(self):
+        return driver.find_element_by_css_selector(".ng-touched").send_keys("tomoz")
+    def passwordField(self):
+        return driver.find_element_by_css_selector("input[name = 'password']").send_keys("tomOz123")
--- tests.py
+++ tests.py
@@ -8,7 +8,6 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.action_chains import ActionChains
 import unittest
 from Pages import mainPage
-
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)
@@ -17,5 +16,15 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         driver = webdriver.Chrome(executable_path='c:/seleniumDriver/chromedriver.exe')
         driver.implicitly_wait(10)
+        self.login()
+
+
+    def login(self):
         driver.get("https://www.advantageonlineshopping.com/")
         driver.maximize_window()
+        driver.find_element_by_css_selector("#menuUser").click()
+        driver.find_element_by_css_selector(".ng-touched").send_keys("tomoz")
+        driver.find_element_by_css_selector("input[name = 'password']").send_keys("tomOz123")
+        driver.find_element_by_css_selector("#sign_in_btnundefined").click()
+
+
